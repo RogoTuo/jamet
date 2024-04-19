485d0d73860b7887560e6c315545b11d356dedc0
Rogo Tuo <bamburumah383@gmail.com>
2024-04-19 11:08:22 +0000

    update

---
+++ node_modules/package-dependents/lib/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var GetDependents = require("npm-pack-dependents"),
+    PackageJson = require("package-json");
+
+/**
+ * PackageDependents
+ * Get the dependents of a given packages. The callback function is called with
+ * an error and an array of objects.
+ *
+ * @name PackageDependents
+ * @function
+ * @param {String} name The package name.
+ * @param {String} version The package version (default: `"latest"`).
+ */
+module.exports = function PackageDependents(name, version) {
+
+    if (typeof version === "function") {
+        callback = version;
+        version = "latest";
+    }
+
+    return GetDependents(name).then(function (packages) {
+        return Promise.all(packages.map(function (c) {
+            return PackageJson(c, version);
+        }));
+    });
+};
